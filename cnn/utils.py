934f00de03cfc5b9e73af05568b093014af3d42b
ShowLo <945519267@qq.com>
2020-05-11 23:52:32 +0800

    add comment

--- cnn/utils.py
+++ cnn/utils.py
@@ -103,6 +103,7 @@ def drop_path(x, drop_prob):
   if drop_prob > 0.:
     keep_prob = 1.-drop_prob
     mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+    # 没有丢弃之前的期望为E[x]，加入drop path之后的期望为p*E[x]，需要除以p保持期望不变
     x.div_(keep_prob)
     x.mul_(mask)
   return x
